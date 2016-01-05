b6d72301c4ef0432d564db704af389796f1d751e
木村正徳 <kimuramasanori@kimuramasanori-no-macbook.local>
2016-01-05 09:32:52 +0900

    Initial Commit

---
+++ YukoMyNumberApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YukoMyNumberApp
+//
+//  Created by 木村正徳 on 2016/01/05.
+//  Copyright © 2016年 yukobex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
