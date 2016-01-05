80bc11f5b6c602f79c8975042f36526beca6d0a6
木村正徳 <kimuramasanori@kimuramasanori-no-macbook.local>
2016-01-05 15:52:26 +0900

    2016/01/05:初回コミット

--- YukoMyNumberApp/RegisterEditViewController.swift
+++ YukoMyNumberApp/RegisterEditViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RegisterEditViewController.swift
 //  YukoMyNumberApp
 //
 //  Created by 木村正徳 on 2016/01/05.
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class RegisterEditViewController:UITableViewController{
+  
+  // MARK: - Table View
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
 }
-
---
+++ YukoMyNumberApp/RegisteredListViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  YukoMyNumberApp
+//
+//  Created by 木村正徳 on 2016/01/05.
+//  Copyright © 2016年 yukobex. All rights reserved.
+//
+
+import UIKit
+
+class RegisteredListViewController: UITableViewController {
+
+  // MARK: テストデータ
+  let registerList = ["広又　太郎","合川　次郎","国分　花子"]
+  
+
+
+  // MARK: - Segues
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "showRegisterEdit" {
+    
+    }
+  }
+  
+  // MARK: - Table View
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  override func numberOfSectionsInTableView(tableView:UITableView) -> Int{
+    return 1
+  }
+
+  override func tableView(tableView:UITableView,numberOfRowsInSection section:Int) -> Int {
+    return registerList.count
+  }
+  
+  override func tableView(tableView:UITableView,cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("RegisteredCell",forIndexPath:indexPath) as UITableViewCell
+    cell.textLabel!.text = self.registerList[indexPath.row]
+    return cell
+  }
+  
+  override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 60
+  }
+  
+  override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    
+    var view = tableView.dequeueReusableHeaderFooterViewWithIdentifier("Header")
+    if view == nil {
+      view = UITableViewHeaderFooterView(reuseIdentifier: "Header") as UITableViewHeaderFooterView
+    }
+    
+    view?.textLabel?.text = "※このアプリで登録した社員のみが表示されます\n※初回登録から1ヶ月経過した登録者は削除されます"
+    return view
+  }
+  
+  
+  
+
+}
+
