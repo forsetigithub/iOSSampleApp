00e904cd316465173048d725b31b9c9a4a4afc04
木村正徳 <kimuramasanori@kimuramasanorinomacbook-pro.local>
2017-03-03 09:31:25 +0900

    2017/03/03:Ver.1.0.3.0(migration)

--- YukoMyNumberApp/RegisteredListViewController.swift
+++ YukoMyNumberApp/RegisteredListViewController.swift
@@ -9,56 +9,69 @@
 import UIKit
 import RealmSwift
 import SVProgressHUD
+// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
+// Consider refactoring the code to use the non-optional operators.
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+  switch (lhs, rhs) {
+  case let (l?, r?):
+    return l < r
+  case (nil, _?):
+    return true
+  default:
+    return false
+  }
+}
+
 
 
 class RegisteredListViewController: UITableViewController {
   
   let realm = try! Realm()
   
-  private let Properties = YukoMyNumberAppProperties.sharedInstance
-  private let employeefilter = NSPredicate(format: "RSCode = '00'")
+  fileprivate let Properties = YukoMyNumberAppProperties.sharedInstance
+  fileprivate let employeefilter = NSPredicate(format: "RSCode = '00'")
   
-  private var isConnectedFlg:Bool = false
+  fileprivate var isConnectedFlg:Bool = false
   
   // MARK: - Table View
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.title = Properties.NavigationTitles["RegisteredListViewController"]
-    self.navigationItem.leftBarButtonItem = editButtonItem()
-    self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: Properties.ButtonTitles["AddNew"], style: UIBarButtonItemStyle.Plain, target: self, action: "tapAddNewButton:")
+    self.navigationItem.leftBarButtonItem = editButtonItem
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: Properties.ButtonTitles["AddNew"], style: UIBarButtonItemStyle.plain, target: self, action: #selector(RegisteredListViewController.tapAddNewButton(_:)))
     
-    let infobutton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Action, target: self, action: "tapVersionInfo:")
+    let infobutton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.action, target: self, action: #selector(RegisteredListViewController.tapVersionInfo(_:)))
     
-    let barspace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FixedSpace, target: nil, action: nil)
+    let barspace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.fixedSpace, target: nil, action: nil)
     barspace.width = self.view.bounds.size.width / 2 - Properties.ToolBarFixedSpaceSize
     self.toolbarItems = [barspace,infobutton]
-    self.navigationController?.toolbarHidden = false
+    self.navigationController?.isToolbarHidden = false
   }
 
   override func viewWillLayoutSubviews() {
 
   }
   
-  override func viewDidAppear(animated: Bool) {
+  override func viewDidAppear(_ animated: Bool) {
   
     SVProgressHUD.show()
   
     let connectioncheck = CheckNetworkConnect(host: Properties.ServerInfo["IPAddress"]!)
   
     if(!self.isConnectedFlg && !connectioncheck.isConnection()) {
-      self.navigationItem.rightBarButtonItem?.enabled = false
-      self.navigationItem.leftBarButtonItem?.enabled = false
-      self.tableView.userInteractionEnabled = false
+      self.navigationItem.rightBarButtonItem?.isEnabled = false
+      self.navigationItem.leftBarButtonItem?.isEnabled = false
+      self.tableView.isUserInteractionEnabled = false
       
-      let myAlert = UIAlertController(title: "ネットワークに\n接続されていません", message: "ネットワークに接続されていないため\nこのアプリを使用することはできません", preferredStyle: UIAlertControllerStyle.Alert)
-      let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Destructive, handler: { (action:UIAlertAction) -> Void in
+      let myAlert = UIAlertController(title: "ネットワークに\n接続されていません", message: "ネットワークに接続されていないため\nこのアプリを使用することはできません", preferredStyle: UIAlertControllerStyle.alert)
+      let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.destructive, handler: { (action:UIAlertAction) -> Void in
         
       })
       
       myAlert.addAction(OKAction)
       
-      self.view.window?.rootViewController?.presentViewController(myAlert, animated: true, completion: nil)
+      self.view.window?.rootViewController?.present(myAlert, animated: true, completion: nil)
     
     }else{
       self.isConnectedFlg = true
@@ -68,7 +81,7 @@ class RegisteredListViewController: UITableViewController {
     SVProgressHUD.dismiss()
   }
   
-  override func viewWillAppear(animated: Bool) {
+  override func viewWillAppear(_ animated: Bool) {
     
     self.tableView.reloadData()
     
@@ -76,9 +89,9 @@ class RegisteredListViewController: UITableViewController {
     
   }
   
-  override func setEditing(editing: Bool, animated: Bool) {
+  override func setEditing(_ editing: Bool, animated: Bool) {
     super.setEditing(editing, animated: animated)
-    tableView.editing = editing
+    tableView.isEditing = editing
   }
   
   override func didReceiveMemoryWarning() {
@@ -90,19 +103,19 @@ class RegisteredListViewController: UITableViewController {
 
   }
   
-  override func numberOfSectionsInTableView(tableView:UITableView) -> Int{
+  override func numberOfSections(in tableView:UITableView) -> Int{
     return 1
   }
 
-  override func tableView(tableView:UITableView,numberOfRowsInSection section:Int) -> Int {
-    return self.realm.objects(EmployeeData).filter(employeefilter).count
+  override func tableView(_ tableView:UITableView,numberOfRowsInSection section:Int) -> Int {
+    return self.realm.objects(EmployeeData.self).filter(employeefilter).count
   }
   
-  override func tableView(tableView:UITableView,cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+  override func tableView(_ tableView:UITableView,cellForRowAt indexPath:IndexPath) -> UITableViewCell {
     
-    let cell = tableView.dequeueReusableCellWithIdentifier("RegisteredCell",forIndexPath:indexPath) as UITableViewCell
+    let cell = tableView.dequeueReusableCell(withIdentifier: "RegisteredCell",for:indexPath) as UITableViewCell
     
-    let employee = self.realm.objects(EmployeeData).filter(employeefilter)[indexPath.row]
+    let employee = self.realm.objects(EmployeeData.self).filter(employeefilter)[indexPath.row]
     
     for subview in cell.contentView.subviews{
       switch subview.tag{
@@ -124,33 +137,33 @@ class RegisteredListViewController: UITableViewController {
     return cell
   }
   
-  override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+  override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
     return 60
   }
   
-  override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+  override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
     
-    var view = tableView.dequeueReusableHeaderFooterViewWithIdentifier("Header")
+    var view = tableView.dequeueReusableHeaderFooterView(withIdentifier: "Header")
     if view == nil {
       view = UITableViewHeaderFooterView(reuseIdentifier: "Header") as UITableViewHeaderFooterView
     }
     
-    view?.textLabel?.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
+    view?.textLabel?.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
     view?.textLabel?.numberOfLines = 2
     view?.textLabel?.text = "※このアプリで登録した従業員のみが表示されます\n※初回登録から1ヶ月経過した登録者は削除されます"
     return view
   }
   
-  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
     return true
   }
   
-  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
     
     SVProgressHUD.show()
     
     //データ削除
-    let employee = self.realm.objects(EmployeeData).filter(employeefilter)[indexPath.row]
+    let employee = self.realm.objects(EmployeeData.self).filter(employeefilter)[indexPath.row]
     let delemployeecode = employee.EmployeeCode
     
     /* データ送信する前に削除した場合、同一の従業員番号はSQLServerのT_Employeeテーブル中に１つしかない。
@@ -170,9 +183,9 @@ class RegisteredListViewController: UITableViewController {
   print("selsql = \(selsql)")
 #endif
             
-            client.execute(selsql, completion: { (results:[AnyObject]!) -> Void in
+            client.execute(selsql, completion: { (results:[Any]?) -> Void in
               
-              if(results[0].count == 1){
+              if((results?[0] as AnyObject).count == 1){
 
                 var updatesql = "update T_EmployeeAffliationRelation set AlreadyUsedFlg = 0 where EmployeeCode = '\(delemployeecode)';"
                 updatesql = updatesql + "update T_Employee set DelFlg = 1 where EmployeeCode = '\(delemployeecode)' and SeqNo = '\(employee.SQLServerSeqNo)';"
@@ -181,17 +194,17 @@ class RegisteredListViewController: UITableViewController {
 #endif
                 
                 
-                client.execute(updatesql, completion: { (results:[AnyObject]!) -> Void in
+                client.execute(updatesql, completion: { (results:[Any]?) -> Void in
                   
                   client.disconnect()
                   
                   try! self.realm.write({ () -> Void in
                     self.realm.delete(employee)
                     
-                    let families = self.realm.objects(EmployeeData).filter("EmployeeCode='\(delemployeecode)'")
+                    let families = self.realm.objects(EmployeeData.self).filter("EmployeeCode='\(delemployeecode)'")
                     self.realm.delete(families)
                     
-                    self.tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: indexPath.row, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Fade)
+                    self.tableView.deleteRows(at: [IndexPath(row: indexPath.row, section: 0)], with: UITableViewRowAnimation.fade)
                   })
                   
                   
@@ -203,10 +216,10 @@ class RegisteredListViewController: UITableViewController {
                 try! self.realm.write({ () -> Void in
                   self.realm.delete(employee)
                   
-                  let families = self.realm.objects(EmployeeData).filter("EmployeeCode='\(delemployeecode)'")
+                  let families = self.realm.objects(EmployeeData.self).filter("EmployeeCode='\(delemployeecode)'")
                   self.realm.delete(families)
                   
-                  self.tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: indexPath.row, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Fade)
+                  self.tableView.deleteRows(at: [IndexPath(row: indexPath.row, section: 0)], with: UITableViewRowAnimation.fade)
                 })
               
                 SVProgressHUD.dismiss()
@@ -218,66 +231,66 @@ class RegisteredListViewController: UITableViewController {
       try! self.realm.write({ () -> Void in
         self.realm.delete(employee)
         
-        let families = self.realm.objects(EmployeeData).filter("EmployeeCode='\(delemployeecode)'")
+        let families = self.realm.objects(EmployeeData.self).filter("EmployeeCode='\(delemployeecode)'")
         self.realm.delete(families)
         
-        self.tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: indexPath.row, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Fade)
+        self.tableView.deleteRows(at: [IndexPath(row: indexPath.row, section: 0)], with: UITableViewRowAnimation.fade)
       })
       
       SVProgressHUD.dismiss()
     }
   }
   
-  override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
     
     return indexPath
   }
   
-  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
   
   }
   
   
-  @IBAction func tapAddNewButton(sender: UIBarButtonItem) {
+  @IBAction func tapAddNewButton(_ sender: UIBarButtonItem) {
   
-    performSegueWithIdentifier("showAddNewEmployee", sender: self)
+    performSegue(withIdentifier: "showAddNewEmployee", sender: self)
   }
   
   // MARK: - Segues
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == "showRegisterEdit" {
-      let employeedata = self.realm.objects(EmployeeData).filter(self.employeefilter)[(self.tableView.indexPathForSelectedRow?.row)!]
+      let employeedata = self.realm.objects(EmployeeData.self).filter(self.employeefilter)[(self.tableView.indexPathForSelectedRow?.row)!]
      
-      let regedit = segue.destinationViewController as? EditEmployeeViewController
+      let regedit = segue.destination as? EditEmployeeViewController
       regedit?.EmployeeEditData = employeedata
     }
   }
   
-  internal func changeTextField(sender:NSNotification){
+  internal func changeTextField(_ sender:Foundation.Notification){
     
     let textField = sender.object as! UITextField
     
-    print(textField.text)
+    print(textField.text ?? " ")
   }
   
   // MARK: 過去データの自動削除
   func deletePastData(){
     
-    let calendar = NSCalendar(identifier: NSCalendarIdentifierGregorian)!
+    let calendar = Calendar(identifier: Calendar.Identifier.gregorian)
     
-    let amonthago = calendar.dateByAddingUnit(NSCalendarUnit.Month, value: (-1) * (Properties.DeleteMonthSpan), toDate: NSDate(), options:NSCalendarOptions.MatchFirst)!
+    let amonthago = (calendar as NSCalendar).date(byAdding: NSCalendar.Unit.month, value: (-1) * (Properties.DeleteMonthSpan), to: Date(), options:NSCalendar.Options.matchFirst)!
     
     print(amonthago)
     
-    let dateformatter = NSDateFormatter()
+    let dateformatter = DateFormatter()
     dateformatter.dateFormat = "yyyyMMddHHmmss"
     
     
-    let alllist = realm.objects(EmployeeData)
+    let alllist = realm.objects(EmployeeData.self)
     var dellist:[EmployeeData] = [EmployeeData]()
     
     for rec in alllist{
-      if(Int(dateformatter.stringFromDate(rec.CreateDateTime)) < Int(dateformatter.stringFromDate(amonthago))){
+      if(Int(dateformatter.string(from: rec.CreateDateTime)) < Int(dateformatter.string(from: amonthago))){
         print("\(rec.EmployeeCode) \(rec.CreateDateTime)")
         dellist.append(rec)
       }
@@ -288,13 +301,13 @@ class RegisteredListViewController: UITableViewController {
     })
   }
   
-  func tapVersionInfo(sender:UIBarButtonItem){
-    let version:AnyObject! = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString")
-    let myAlert = UIAlertController(title: "MN登録\nVer.\(version)", message: "YUKO BEX Inc. All Rights Reserved.", preferredStyle: UIAlertControllerStyle.ActionSheet)
-    let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default,handler:nil)
+  func tapVersionInfo(_ sender:UIBarButtonItem){
+    let version:String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+    let myAlert = UIAlertController(title: "MN登録\nVer.\(version)", message: "YUKO BEX Inc. All Rights Reserved.", preferredStyle: UIAlertControllerStyle.actionSheet)
+    let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler:nil)
     
     myAlert.addAction(OKAction)
-    presentViewController(myAlert, animated: true, completion: nil)
+    present(myAlert, animated: true, completion: nil)
   }
 }
 
