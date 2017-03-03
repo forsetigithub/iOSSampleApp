b80748c1206dbb823a069f9a59131cab5dd61075
木村正徳 <kimuramasanori@kimuramasanorinomacbook-pro.local>
2017-03-03 09:31:54 +0900

    2017/03/03:Ver.1.0.3.0(migration)

--- YukoMyNumberApp/RegisterEmployeeViewController.swift
+++ YukoMyNumberApp/RegisterEmployeeViewController.swift
@@ -26,7 +26,7 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
   @IBOutlet weak var JoinedDatePicker: UIDatePicker!
 
   var JoinedDate:NSDate?
-  let formatter = NSDateFormatter()
+  let formatter = DateFormatter()
   
   private var InitialJoinedDateLabel:String!
   private var JoinedDateTappedFlag:Bool = false
@@ -43,9 +43,10 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
 
     
     self.navigationItem.title = Properties.NavigationTitles["RegisterEmployeeViewController"]
-    self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: Properties.ButtonTitles["Register"], style: UIBarButtonItemStyle.Done, target: self, action: "tapRegisterButton:")
-    self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: Properties.ButtonTitles["Cancel"], style: UIBarButtonItemStyle.Plain, target: self, action: "tapCancelButton:")
+
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: Properties.ButtonTitles["Register"], style: UIBarButtonItemStyle.done, target: self, action:#selector(self.tapRegisterButton(sender:)))
     
+    self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: Properties.ButtonTitles["Cancel"], style: UIBarButtonItemStyle.plain, target: self, action:#selector(self.tapCancelButton(sender:)))
     
     self.EmployeeCode.placeholder = Properties.LabelItems["EmployeeCode"]
     self.EmployeeFamilyName.placeholder = Properties.LabelItems["FamilyName"]
@@ -53,20 +54,21 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
     
     formatter.dateFormat = Properties.DateFormatStringSeparetedJapanese
     self.InitialJoinedDateLabel = self.EmployeeJoinedDateLabel.text
-    self.JoinedDatePicker.locale = NSLocale(localeIdentifier: Properties.LocaleIdentifier)
+    self.JoinedDatePicker.locale = NSLocale(localeIdentifier: Properties.LocaleIdentifier) as Locale
     
-    let tapgesture = UITapGestureRecognizer(target: self, action: "EmployeeJoinedDateLabel:")
+    let tapgesture = UITapGestureRecognizer(target: self, action: #selector(self.tapEmployeeJoinedDateLabel(sender:)))
+    self.EmployeeJoinedDateLabel.isUserInteractionEnabled = true
     self.EmployeeJoinedDateLabel.addGestureRecognizer(tapgesture)
     self.EmployeeJoinedDateLabel.text = Properties.JoinedDateLabelTapComment
     
   }
   
-  override func viewWillAppear(animated: Bool) {
+  override func viewWillAppear(_ animated: Bool) {
     
-    self.navigationController?.toolbarHidden = true
+    self.navigationController?.isToolbarHidden = true
   }
   
-  override func viewDidAppear(animated: Bool) {
+  override func viewDidAppear(_ animated: Bool) {
     if(self.EmployeeCode.text?.isEmpty == true){
       self.EmployeeCode.becomeFirstResponder()
     }
@@ -77,34 +79,36 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
     // Dispose of any resources that can be recreated.
   }
 
-  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     return 3
   }
   
-  override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-    var height:CGFloat = Properties.TableViewCellDefaultHeight
-    
-    if(indexPath.section == 2 && indexPath.row == 1){
-      if(self.JoinedDateTappedFlag == true){
-        height = 150
-        self.EmployeeJoinedDateLabel.text = formatter.stringFromDate(self.JoinedDatePicker.date)
-      }else{
-        height = 0
-      }
-    }
-    
-    return height
-  }
 
-  func textFieldDidEndEditing(textField: UITextField) {
+   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var height:CGFloat = Properties.TableViewCellDefaultHeight
+        
+        if(indexPath.section == 2 && indexPath.row == 1){
+            if(self.JoinedDateTappedFlag == true){
+                height = 150
+                self.EmployeeJoinedDateLabel.text = formatter.string(from: self.JoinedDatePicker.date)
+            }else{
+                height = 0
+            }
+        }
+        
+        return height
+    }
+
+    
+  func textFieldDidEndEditing(_ textField: UITextField) {
 
   }
   
-  func textFieldShouldReturn(textField: UITextField) -> Bool {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
     return textField.resignFirstResponder()
   }
   
-  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
     
     let str = textField.text! + string
     
@@ -130,47 +134,47 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
     return true
   }
   
-  func EmployeeJoinedDateLabel(sender: UILabel) {
+  func tapEmployeeJoinedDateLabel(sender: UITapGestureRecognizer) {
     self.JoinedDateTappedFlag = !(self.JoinedDateTappedFlag)
     
     if(self.JoinedDateTappedFlag == true){
-      self.EmployeeJoinedDateLabel.textColor = UIColor.blueColor()
+      self.EmployeeJoinedDateLabel.textColor = UIColor.blue
       
     }else{
-      self.EmployeeJoinedDateLabel.textColor = UIColor.blackColor()
+      self.EmployeeJoinedDateLabel.textColor = UIColor.black
     }
     
-    self.JoinedDate = self.JoinedDatePicker.date
+    self.JoinedDate = self.JoinedDatePicker.date as NSDate?
     self.tableView.reloadData()
 
   }
   
   @IBAction func changeDatePickerValue(sender: UIDatePicker) {
     
-    self.JoinedDate = sender.date
-    self.EmployeeJoinedDateLabel.text = self.formatter.stringFromDate(self.JoinedDate!)
+    self.JoinedDate = sender.date as NSDate?
+    self.EmployeeJoinedDateLabel.text = self.formatter.string(from: self.JoinedDate! as Date)
     
   }
   
   
-  @IBAction func tapRegisterButton(sender: UIBarButtonItem) {
+  internal func tapRegisterButton(sender: UIBarButtonItem) {
 
     //必須入力チェック
-    if(self.EmployeeCode.text?.stringByReplacingOccurrencesOfString(" ", withString: "").isEmpty == true ||
-      self.EmployeeFamilyName.text?.stringByReplacingOccurrencesOfString(" ", withString: "").isEmpty == true ||
-      self.EmployeeFirstName.text?.stringByReplacingOccurrencesOfString(" ", withString: "").isEmpty == true  ||
+    if(self.EmployeeCode.text?.replacingOccurrences(of: " ", with: "").isEmpty == true ||
+      self.EmployeeFamilyName.text?.replacingOccurrences(of: " ", with: "").isEmpty == true ||
+      self.EmployeeFirstName.text?.replacingOccurrences(of: " ", with: "").isEmpty == true  ||
       self.EmployeeJoinedDateLabel.text == Properties.JoinedDateLabelTapComment ){
         
         let requiredvalid:[String:String] = Properties.AlertMessages["RequiredItemValidError"] as! [String:String]
         
-        let myAlert = UIAlertController(title: requiredvalid["Title"], message: requiredvalid["Message"], preferredStyle: UIAlertControllerStyle.Alert)
-        let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Destructive, handler: { (action:UIAlertAction) -> Void in
+        let myAlert = UIAlertController(title: requiredvalid["Title"], message: requiredvalid["Message"], preferredStyle: UIAlertControllerStyle.alert)
+        let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.destructive, handler: { (action:UIAlertAction) -> Void in
           
         })
         
         myAlert.addAction(OKAction)
         
-        self.presentViewController(myAlert, animated: true, completion: nil)
+        self.present(myAlert, animated: true, completion: nil)
         
         self.resignFirstResponder()
         
@@ -182,10 +186,10 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
   }
 
   @IBAction func tapCancelButton(sender: UIBarButtonItem) {
-    self.dismissViewControllerAnimated(true, completion: nil)
+    self.dismiss(animated: true, completion: nil)
   }
   
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 
   }
   
@@ -200,24 +204,24 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
     NewEmployeeData.RSCode = "00"
     
     if let joineddate = self.JoinedDate{
-      NewEmployeeData.JoinedDate = joineddate
+      NewEmployeeData.JoinedDate = joineddate as Date
     }
     
-    NewEmployeeData.CreateDateTime = NSDate()
+    NewEmployeeData.CreateDateTime = NSDate() as Date
   
-    if(realm.objects(EmployeeData).filter("EmployeeCode = '\(NewEmployeeData.EmployeeCode)'").count != 0){
+    if(realm.objects(EmployeeData.self).filter("EmployeeCode = '\(NewEmployeeData.EmployeeCode)'").count != 0){
       
       let doubleerror:[String:String] = Properties.AlertMessages["DoubleCheckError"] as! [String:String]
       
-      let myAlert = UIAlertController(title: doubleerror["Title"]!, message: "入力した\(Properties.LabelItems["EmployeeCode"]!)は\n\(doubleerror["Message"]!)", preferredStyle: UIAlertControllerStyle.Alert)
-      let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Destructive, handler: nil)
+      let myAlert = UIAlertController(title: doubleerror["Title"]!, message: "入力した\(Properties.LabelItems["EmployeeCode"]!)は\n\(doubleerror["Message"]!)", preferredStyle: UIAlertControllerStyle.alert)
+      let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.destructive, handler: nil)
       
       myAlert.addAction(OKAction)
-      presentViewController(myAlert, animated: true, completion: nil)
+      present(myAlert, animated: true, completion: nil)
       return
     }
     
-    uploadData(NewEmployeeData)
+    uploadData(uploaddata: NewEmployeeData)
 
   }
   
@@ -237,26 +241,28 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
           
           print("Connection Successed!")
           
-          let dateformatter = NSDateFormatter()
+          let dateformatter = DateFormatter()
           dateformatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
           
-          let timestamp = dateformatter.stringFromDate(NSDate())
+          let timestamp = dateformatter.string(from: NSDate() as Date)
           
           var checksqlstring = "SELECT A0.EmployeeCode as A0_EmployeeCode,AffliationCode,AlreadyUsedFlg,MyNumberRegistedFlg,B0.EmployeeCode as B0_EmployeeCode" +
                               " FROM T_EmployeeAffliationRelation as A0 left join T_Employee as B0 on " +
                               "A0.EmployeeCode = B0.EmployeeCode where A0.EmployeeCode= '\(uploaddata.EmployeeCode)'"
           
-          self.client.execute(checksqlstring, completion: { (results:[AnyObject]!) -> Void in
-            
-            if(results[0].count == 0){
+          self.client.execute(checksqlstring, completion: { (results:[Any]?) -> Void in
+
+            if((results?[0] as AnyObject).count == 0){
               
-              self.putAlertMessage(self.Properties.AlertMessages["NotRegisteredEmployeeCodeError"])
+              self.putAlertMessage(alertProp: self.Properties.AlertMessages["NotRegisteredEmployeeCodeError"])
 
             }else{
-              if((results[0][0]["AlreadyUsedFlg"] as! NSString).intValue == 1 &&
-                (results[0][0]["MyNumberRegistedFlg"] as! NSString).intValue == 0){
+              let selectedData = (results?[0] as! NSArray?)?[0] as AnyObject?
                 
-                self.putAlertMessage(self.Properties.AlertMessages["DoubleCheckError"])
+              if((selectedData!["AlreadyUsedFlg"] as! NSString).intValue == 1 &&
+                (selectedData!["MyNumberRegistedFlg"] as! NSString).intValue == 0){
+                
+                self.putAlertMessage(alertProp: self.Properties.AlertMessages["DoubleCheckError"])
 
               }else{
                 var sqlstring = "insert into T_Employee(" +
@@ -268,7 +274,7 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
                   "'\(uploaddata.RSCode)'," +
                   "'\(uploaddata.FamilyName)'," +
                   "'\(uploaddata.FirstName)'," +
-                  "'\(dateformatter.stringFromDate(uploaddata.JoinedDate))'," +
+                  "'\(dateformatter.string(from: uploaddata.JoinedDate))'," +
                   "'\(uploaddata.MyNumber)'," +
                   "'\(timestamp)'" +
                 ");"
@@ -279,33 +285,35 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
                 
                 print(sqlstring)
                 
-                self.client.execute(sqlstring, completion: { (results:[AnyObject]!) -> Void in
+                self.client.execute(sqlstring, completion: { (results:[Any]?) -> Void in
                   
                   checksqlstring = "select * from T_Employee where EmployeeCode = '\(uploaddata.EmployeeCode)' and TimeStamp = '\(timestamp)'"
                   
                   print(checksqlstring)
                   
-                  self.client.execute(checksqlstring, completion: { (results:[AnyObject]!) -> Void in
+                  self.client.execute(checksqlstring, completion: { (results:[Any]?) -> Void in
                     
-                    if(results[0].count != 0){
+                    if((results?[0] as AnyObject).count != 0){
                       //データ登録
                       try! self.realm.write({ () -> Void in
-                        uploaddata.SQLServerSeqNo = results[0][0]["SeqNo"] as! String
+                        let selectedData = (results?[0] as! NSArray)[0] as AnyObject
+                        
+                        uploaddata.SQLServerSeqNo = selectedData["SeqNo"] as! String
                         self.realm.add(uploaddata)
                       })
                       
-                      self.dismissViewControllerAnimated(true, completion: nil)
+                      self.dismiss(animated: true, completion: nil)
                       
                     }else{
                       
                       let uploaderror:[String:String] = self.Properties.AlertMessages["UploadNotCompleteError"] as! [String:String]
-                      let messageAlert = UIAlertController(title: uploaderror["Title"], message:"入力した\(self.Properties.LabelItems["EmployeeCode"])は\(uploaderror["Message"])" , preferredStyle: UIAlertControllerStyle.Alert)
-                      let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Destructive, handler: { (action:UIAlertAction) -> Void in
+                      let messageAlert = UIAlertController(title: uploaderror["Title"], message:"入力した\(self.Properties.LabelItems["EmployeeCode"])は\(uploaderror["Message"])" , preferredStyle: UIAlertControllerStyle.alert)
+                      let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.destructive, handler: { (action:UIAlertAction) -> Void in
                         
                       })
                       
                       messageAlert.addAction(OKAction)
-                      self.presentViewController(messageAlert, animated: true, completion: nil)
+                      self.present(messageAlert, animated: true, completion: nil)
                     }
                     
                     self.client.disconnect()
@@ -326,7 +334,7 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
     }
   }
   
-  func error(error: String!, code: Int32, severity: Int32) {
+  func error(_ error: String!, code: Int32, severity: Int32) {
     print("error = \(error),code = \(code),severity = \(severity)")
   }
   
@@ -335,12 +343,12 @@ class RegisterEmployeeViewController : UITableViewController,UITextFieldDelegate
     
     SVProgressHUD.dismiss()
     
-    let myAlert = UIAlertController(title: alertProp["Title"]!, message: alertProp["Message"]!, preferredStyle: UIAlertControllerStyle.Alert)
-    let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Destructive, handler: { (action:UIAlertAction) -> Void in
+    let myAlert = UIAlertController(title: alertProp["Title"]!, message: alertProp["Message"]!, preferredStyle: UIAlertControllerStyle.alert)
+    let OKAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.destructive, handler: { (action:UIAlertAction) -> Void in
       
     })
     
     myAlert.addAction(OKAction)
-    self.presentViewController(myAlert, animated: true, completion: nil)
+    self.present(myAlert, animated: true, completion: nil)
   }
 }
